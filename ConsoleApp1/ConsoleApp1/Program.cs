14e0df8bb3d621bf131e18bb43b6e4384245dc73
Błażej Kujawiński <sxxxx@pjwstk.edu.pl>
2024-03-12 11:30:38 +0100

    add Feature: Get Average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,3 +2,20 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Zmiana nr 1");
+
+
+static double GetAverage(int [] tab)
+{
+    double srednia;
+    double suma = 0;
+    for (int i = 0; i < tab.Length; i++)
+    {
+        suma += tab[i];
+        
+    }
+
+    srednia = suma / tab.Length;
+
+    return srednia;
+}
+
